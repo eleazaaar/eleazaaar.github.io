961998aeefea0f3f9c096ed86be09ce6dbf1884a
Eleazar Franco <132207007+eleazaaar@users.noreply.github.com>
2023-05-14 16:32:30 +0800

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
 function displayProjects() {
-    $("#displayProject").css({ "background-color": "white", "color": "black" });
+    resetAttributes();
+    $("#btnProjects").css({ "background-color": "white", "color": "black" });
     $("#projects").show();
-    $("#activities").hide();
 }
 
 function displayActivities() {
+    resetAttributes();
+    $("#btnActivities").css({ "background-color": "white", "color": "black" });
     $("#activities").show();
+}
+
+function displaySeminars() {
+    resetAttributes();
+    $("#btnSeminars").css({ "background-color": "white", "color": "black" });
+    $("#seminars").show();
+}
+
+function resetAttributes() {
+    $("#btnProjects").css({ "background-color": "rgba(0, 0, 0, 0)", "color": "white" });
+    $("#btnActivities").css({ "background-color": "rgba(0, 0, 0, 0)", "color": "white" });
+    $("#btnSeminars").css({ "background-color": "rgba(0, 0, 0, 0)", "color": "white" });
     $("#projects").hide();
+    $("#activities").hide();
+    $("#seminars").hide();
 }
